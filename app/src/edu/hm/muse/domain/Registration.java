249a13abe175383b83f4babaf4cd2f9d2dc43f77
MoritzKorsch <Moritz.Korsch@gmail.com>
2016-07-08 08:25:57 +0200

    Registration should be done

--- app/src/edu/hm/muse/domain/Registration.java
+++ app/src/edu/hm/muse/domain/Registration.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import authentication.Authentication;
+import authentication.SaltAndPasswordShaker;
 import authentication.Token;
 import stuff.PatternChecker;
 import stuff.SessionInfo;
@@ -34,39 +35,47 @@ public class Registration {
 	}
 	
 	@RequestMapping(value = "/registration.secu", method = RequestMethod.POST)
-	public ModelAndView register(@RequestParam(value = "mname", required = false) String mname,
-			@RequestParam(value = "mpwd", required = false) String mpwd,
-			@RequestParam(value = "vpwd", required = false) String vpwd,
+	public ModelAndView register(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "pass", required = false) String pass,
+			@RequestParam(value = "passConf", required = false) String passConf,
 			@RequestParam(value = "Token", required = false) Token token, HttpSession session, SessionInfo sessionInfo) {
 		
-		ModelAndView mv = new ModelAndView("registration");
 		
-		if (mname == null || mpwd == null || vpwd == null || mname == "" || mpwd == "" || vpwd == "" 
-				|| !mpwd.equals(vpwd)) {
-			return returnToRegistration(session,"Something went wrong...");
+		if (name == null || pass == null || passConf == null || name == "" || pass == "" || passConf == "" 
+				|| !pass.equals(passConf)) {
+			return returnToRegistration(session,"No empty fields allowed!");
 		}
 		
-		else if (!(new PatternChecker(mname).checkUserName())) return returnToRegistration(session, "Please don't use fancy symbols in the username!");
-		else if (!(new PatternChecker(mpwd).checkPassword())) return returnToRegistration(session, "Please don't use fancy symbols in the password!");
+		else if (!(new PatternChecker(name).checkUserName())) return returnToRegistration(session, "Please don't use fancy symbols in the username!");
+		else if (!(new PatternChecker(pass).checkPassword())) return returnToRegistration(session, "Please don't use fancy symbols in the password!");
 		
 		else if (null == token || !(new Authentication().authenticateToken((Token) session.getAttribute("Token"), token))) {
         	return returnToRegistration(session, "Authentication error!");
         }
 		else {
-			String sql = "SELECT count(*) FROM M_USER WHERE muname = ?";
+			String sqlCount = "SELECT count(*) FROM M_USER WHERE muname = ?";
 			int temp = 0;
 			try {
-				temp = jdbcTemplate.queryForInt(sql, new Object[]{mname});
+				temp = jdbcTemplate.queryForInt(sqlCount, new Object[]{name});
 			} catch (Exception e) {
 				return returnToRegistration(session, "Something went wrong...");
-			} finally {
-				if (temp > 0) return returnToRegistration(session, "User already exists");
-				
-				
-				//TODO insert User with salt and hashed password
+			} 
+
+			if (temp > 0) return returnToRegistration(session, "User already exists");
+			
+			String sqlInsertUser = "INSERT INTO USER(name, pwd, salt) VALUES(?, ?, ?)";
+			SaltAndPasswordShaker snp = new SaltAndPasswordShaker();
+			try {
+				jdbcTemplate.update(sqlInsertUser, new Object[] {name, snp.byteToString(snp.generate())});
+			} catch (Exception e) {
+				return returnToRegistration(session, "Something bad happened...");
 			}
 		}
 		
+		//no instant sign in for security reasons
+		ModelAndView mv = new ModelAndView("login");
+		mv.addObject("msg", "Login");
+		mv.addObject("isLoggedIn", sessionInfo.isLoggedIn(session));
 		
 		return mv;
 	}
